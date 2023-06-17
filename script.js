f6629114d3fedec282b49c5902440e60be851cc8
rileyham <rileyham21@gmail.com>
2023-06-17 00:18:07 -0400

    Add functionality to New Canvas button

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const container = document.getElementById("container");
+const canvasButton = document.getElementById("newCanvas");
+let cells = document.getElementsByClassName("cell");
+let rows = document.getElementsByClassName("row");
 
+// draws a grid of cells of a given size
 function drawCells(cellCount) {
 
     for(let i = 0; i < cellCount; i++) {
@@ -14,11 +18,44 @@ function drawCells(cellCount) {
         }
         container.appendChild(newRow);
     }
+    cells = document.getElementsByClassName("cell");
+    rows = document.getElementsByClassName("row");
+
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener("mouseenter", function () {
+            cells[i].classList.add("cell-hover");
+        });
+        /*
+        cells[i].addEventListener("mouseout", function () {
+            cells[i].classList.remove("cell-hover");
+        });
+        */
+    }
 }
 
-drawCells(16);
+// deletes existing cells
+function removeCells () {
 
-let cells = document.getElementsByClassName("cell")
+    for (let i = cells.length - 1; i >= 0; i--) {
+        cells[i].remove()
+    }
+    for (let i = rows.length - 1; i >= 0; i--) {
+        rows[i].remove()
+    }
+}
+
+drawCells(16); // draw starting canvas
+
+canvasButton.addEventListener("click", function() {
+    let newCanvasSize = prompt("Please enter the size of your new canvas.");
+    while(newCanvasSize >= 100) {
+        newCanvasSize = prompt("Canvas sizes must be smaller than 100 cells.");
+    }
+    removeCells();
+    drawCells(newCanvasSize);
+
+});
+
 for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener("mouseenter", function () {
         cells[i].classList.add("cell-hover");
